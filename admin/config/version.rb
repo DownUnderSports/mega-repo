fe30dc1993d0037104051e15a4ea85d0e88b991f
Sampson Crowley <sampsonsprojects@gmail.com>
2020-05-26 18:34:01 -0600

    bump version

--- admin/config/version.rb
+++ admin/config/version.rb
@@ -1,4 +1,4 @@
 module DownUnderSports
   # `git log -1 --date=short --format="%ad-%H"|sed 's/-/./g'`.strip
-  VERSION = '2020.05.26.980fb6e10c7e9bbafff00afb54044b71ebf5afd6'
+  VERSION = '2020.05.26.e98a52a0c98ac7e2b3704fe106ea35b69e8f52cd'
 end
