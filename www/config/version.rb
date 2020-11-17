b495d47976cf80f769fee96097dc89d7e4f02407
Sampson Crowley <sampsonsprojects@gmail.com>
2020-11-17 13:41:29 -0700

    fix insurance paid amount calculator

--- www/config/version.rb
+++ www/config/version.rb
@@ -1,4 +1,4 @@
 module DownUnderSports
   # `git log -1 --date=short --format="%ad-%H"|sed 's/-/./g'`.strip
-  VERSION = '2020.11.17.ee36301455b96e1d3758ff7000dcd3d31860e8dd'
+  VERSION = '2020.11.17.3460832502f40455a06696763efd41ae15a201c4'
 end
