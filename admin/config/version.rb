432741d973fd09eef64def0ecee67409a264461c
Sampson Crowley <sampsonsprojects@gmail.com>
2020-11-30 14:22:04 -0700

    add release form data to travelers download

--- admin/config/version.rb
+++ admin/config/version.rb
@@ -1,4 +1,4 @@
 module DownUnderSports
   # `git log -1 --date=short --format="%ad-%H"|sed 's/-/./g'`.strip
-  VERSION = '2020.11.24.acc4b55d247904acbad5eb470b819320e5cce717'
+  VERSION = '2020.11.24.492df09c2addcb43d5a1519f99e4b41557a30a1a'
 end
