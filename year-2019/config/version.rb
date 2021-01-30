a8772ecc905c20f7b9445fb153c435a340804ef7
Sampson Crowley <sampsonsprojects@gmail.com>
2021-01-29 20:45:09 -0700

    revert qr-code logo

--- year-2019/config/version.rb
+++ year-2019/config/version.rb
@@ -1,4 +1,4 @@
 module DownUnderSports
   # `git log -1 --date=short --format="%ad-%H"|sed 's/-/./g'`.strip
-  VERSION = '2021.01.29.8ff2dea895bbeaf02cc6298330a61ad441eda364'
+  VERSION = '2021.01.29.68e14aef67b806fb4c94efac095468fbc7f0ec12'
 end
